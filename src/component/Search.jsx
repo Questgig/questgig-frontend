0e9ec7239144c2664b3d2b8cdba837e1c953e745
learnwithalex <90308279+learnwithalex@users.noreply.github.com>
2023-05-26 10:56:34 +0000

    New Commit

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -11,7 +11,7 @@ const Search = ({setData , setLoading, onchange , onsubmit}) => {
     
   return (
     <div className='flex  my-10   justify-center items-center '>
- <div className='search flex px-5 text-slate-600  border border-blue-400  rounded-lg items-center p-3 mx-3  lg:w-[900px] '>
+ <div className='search flex px-4 text-slate-600  border border-blue-400  rounded-lg items-center p-3 mx-3  lg:w-[900px] '>
     
   <form action="" className='outline-none border-none flex-grow' onSubmit={onsubmit} >
   <input type="text"className='outline-none border-none flex-grow'  onChange={onchange} />
